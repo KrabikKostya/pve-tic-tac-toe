7bfbde9ef61407a424b5e22f70b4aef9287df19c
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-09-13 13:04:51 +0300

    delated useless files

--- gitscript.sh
+++
@@ -1,7 +0,0 @@
-echo "# pve-tic-tac-toe" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/KrabikKostya/pve-tic-tac-toe.git
-git push -u origin main
